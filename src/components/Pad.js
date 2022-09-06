e94249e23309deaeda247ed1903afdc75b176204
banjich <banjich@gmail.com>
2022-09-06 13:19:59 +0200

    Updated

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -20,21 +20,21 @@ const Pad = () => {
 
   const onEnter = () => {
     if (inputPin === passPin) {
-      setMessage('SUCCESS!');
+      setMessage('-SUCCESS-');
       setInputPin('');
       setDisabled(true);
       setTimeout(() => {
         setDisabled(false);
         setMessage('');
         setAttemptCount(0);
-      }, 1000);
+      }, 1500);
     } else {
       if (inputPin !== passPin && attemptCount !== 2) {
         if (inputPin !== '') {
           setAttemptCount(attemptCount + 1);
           setDisabled(true);
           setInputPin('');
-          setMessage('WRONG PIN!');
+          setMessage('-WRONG PIN-');
           setTimeout(() => {
             setMessage('');
             setDisabled(false);
@@ -43,7 +43,7 @@ const Pad = () => {
       }
       if (inputPin.length === 4 && inputPin !== passPin && attemptCount === 2) {
         setAttemptCount(attemptCount + 1);
-        setMessage('LOCKED!');
+        setMessage('-LOCKED-');
         setInputPin('');
         setDisabled(true);
         setTimeout(() => {
